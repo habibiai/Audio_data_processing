8c0106ef3758e1d5b005f9c46990da648a4445cf
habibiai <habibi.saber70@gmail.com>
2021-11-27 14:23:57 +0330

    upload codes

---
+++ MLP/3_mlp_dropout_regularization.py
@@ -0,0 +1,90 @@
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+
+# path to json file that stores MFCCs and genre labels for each processed segment
+DATA_PATH = "data_10.json"
+
+def load_data(data_path):
+    """Loads training dataset from json file.
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    # convert lists to numpy arrays
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+
+    print("Data succesfully loaded!")
+
+    return  X, y
+
+
+def plot_history(history):
+    fig,ax = plt.subplots(2)
+
+    ax[0].plot(history.history['accuracy'], label = 'train acc')
+    ax[0].plot(history.history['val_accuracy'], label = 'test acc')
+    ax[0].set_ylabel('Acc')
+    ax[1].set_xlabel('Epoch')
+    ax[0].legend(loc = "lower right")
+    ax[0].set_title('Acc eval')
+
+    ax[1].plot(history.history['loss'], label = 'train loss')
+    ax[1].plot(history.history['val_loss'], label = 'test loss')
+    ax[1].set_ylabel('Loss')
+    ax[1].set_xlabel('Epoch')
+    ax[1].legend(loc = "lower right")
+    ax[1].set_title('loss eval')
+
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    # load data
+    X, y = load_data(DATA_PATH)
+
+    # create train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    # build network topology
+    model = keras.Sequential([
+
+        # input layer
+        keras.layers.Flatten(input_shape=(X.shape[1], X.shape[2])),
+
+        # 1st dense layer
+        keras.layers.Dense(512, activation='relu', kernel_regularizer= keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
+
+        # 2nd dense layer
+        keras.layers.Dense(256, activation='relu', kernel_regularizer= keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
+
+        # 3rd dense layer
+        keras.layers.Dense(64, activation='relu', kernel_regularizer= keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
+
+        # output layer
+        keras.layers.Dense(10, activation='softmax')
+    ])
+
+    # compile model
+    optimiser = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(optimizer=optimiser,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    # train model
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=8, epochs=100)
+
+    plot_history(history)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Audio_data_processing
